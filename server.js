0b1e1da51e50adb6fe4e480e29d5c0293637f3a5
Karina Yuja <66886321+KariLinux@users.noreply.github.com>
2024-03-06 01:23:47 -0500

    Added register business API

--- server.js
+++ server.js
@@ -85,6 +85,42 @@ app.post('/api/login', async (req, res, next) => {
 });
 
 
+app.post('/api/registerBusiness', async (req, res, next) => {
+  // incoming: businessName
+  // outgoing: businessId, error
+
+  // Init. error var 
+  var error = '';
+  const { businessName } = req.body;
+
+  // Connect to database 
+  const db = client.db("inventory_tracker");
+
+  try {
+    // Create a new business object
+    const result = await db.collection('businesses').insertOne({
+      businessName: businessName,
+      employeeIdList: [],
+      itemList: [],
+      distributorList: [],
+    });
+
+    // Extract the inserted business ID
+    const businessId = result.insertedId;
+
+    // Return the business ID and no error
+    return res.status(200).json({ businessId, error: '' });
+  }
+
+  catch (error) {
+    console.error("Error during business registration:", error);
+
+    // Return an empty business ID and an error message
+    return res.status(500).json({ businessId: '', error: "Unknown Error" });
+  }
+});
+
+
 
 
 // TODO: REMOVE (Currently to be used as reference)
